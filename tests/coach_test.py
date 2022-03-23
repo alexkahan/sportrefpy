093f8a486fb300cd5ba5427dd2d8650a376053ed
alexkahan <amkahan@gmail.com>
2022-03-23 19:09:59 -0400

    reorganize the app

--- tests/coach_test.py
+++ tests/coach_test.py
@@ -1,10 +1,6 @@
 import pytest
-from sports_stats.nba import NBAFranchise
-# from sports_stats.nhl import NHL, NHLFranchise
-# from sports_stats.nfl import NFL, NFLFranchise
-# from sports_stats.mlb import MLB, MLBFranchise
-# from sports_stats.cbb import CBB, CBBSchool
-# from sports_stats.cfb import CFB, CFBSchool
+from sports_stats.nba.nba import NBAFranchise
+
 
 def test_NBA_coach_playoffs():
     sixers = NBAFranchise('PHI')
--- tests/team_test.py
+++ tests/team_test.py
@@ -1,10 +1,10 @@
 import pytest
-from sports_stats.nba import NBA, NBAFranchise
-from sports_stats.nhl import NHL, NHLFranchise
-from sports_stats.nfl import NFL, NFLFranchise
-from sports_stats.mlb import MLB, MLBFranchise
-from sports_stats.cbb import CBB, CBBSchool
-from sports_stats.cfb import CFB, CFBSchool
+from sports_stats.nba.nba import NBA, NBAFranchise
+from sports_stats.nhl.nhl import NHL, NHLFranchise
+from sports_stats.nfl.nfl import NFL, NFLFranchise
+from sports_stats.mlb.mlb import MLB, MLBFranchise
+from sports_stats.cbb.cbb import CBB, CBBSchool
+from sports_stats.cfb.cfb import CFB, CFBSchool
 
 def test_NBA():
     nba = NBA()
