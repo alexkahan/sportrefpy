970629a22da6142c3fd7ca5c43e6c883831ee746
alexkahan <amkahan@gmail.com>
2022-03-23 19:04:39 -0400

    Add historical data and current roster

--- sports_stats/nba.py
+++ sports_stats/nba.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 class NBA:   
 
@@ -31,24 +32,61 @@ class NBAFranchise(NBA):
         self.franchise = self.teams[franchise]['team_name']
         self.team_url = self.teams[franchise]['url']
         self.players_url = self.team_url + 'players.html'
+        self.coaches_url = self.team_url + 'coaches.html'
+        self.current_roster_url = f'{self.url}/contracts/{self.abbreviation}.html'
 
     
-    def players_all_time(self):
-        self.players = {}
+    def players_all_time_stats(self):
+        '''
+        Returns Pandas dataframe of all historical player data.
+        '''
 
-        response = requests.get(self.players_url)
-        soup = BeautifulSoup(response.text, features='lxml')
+        players = pd.read_html(self.players_url)[0]
+        players.columns = ['Rank', 'Player', 'From', 'To', 'Yrs', 'G', 'MP', \
+            'FG', 'FGA', '3P', '3PA', 'FT', 'FTA', 'ORB', 'TRB', 'AST', \
+            'STL', 'BLK', 'TOV', 'PF', 'PTS', 'FG%', '3P%', 'FT%', 'MPG',\
+            'PPG', 'RBG', 'APG']
+        players.dropna(axis='rows', subset='Player', inplace=True)
+        players.drop(columns={'Rank'}, inplace=True)
+        players = players[players['Player'] != 'Player']
+        players.set_index('Player', inplace=True)
+        players = players.apply(pd.to_numeric)
 
-        for item in soup.find_all(attrs={'data-stat': 'player'})[1:3]:
-            self.players[item.text] = {
-                "url": self.url + item.find('a')['href'],
-            }
+        return players
+
+
+    def coaches_all_time_data(self):
+        '''
+        Returns Pandas dataframe of all historical coach data.
+        '''
+
+        coaches = pd.read_html(self.coaches_url)[0]
+        coaches.columns = ['Rank', 'Coach', 'From', 'To', 'Yrs', 'G', 'W', \
+            'L', 'W/L%', 'W > .500', 'Playoffs', 'Playoff G', 'Playoff W', \
+            'Playoff L', 'Playoff W/L%', 'Conf', 'Champ']
+        coaches.dropna(axis='rows', subset='Coach', inplace=True)
+        coaches.drop(columns={'Rank'}, inplace=True)
+        coaches = coaches[coaches['Coach'] != 'Coach']
+        coaches.set_index('Coach', inplace=True)
+        coaches = coaches.apply(pd.to_numeric)
+
+        return coaches
+
+
+    def current_roster(self):
+        '''
+        Returns Pandas dataframe of current roster.
+        '''
+
+        current_roster = pd.read_html(self.current_roster_url)[0]
+        current_roster = current_roster[current_roster.columns[:2]]
+        current_roster.columns = ['Player', 'Age']
+        current_roster = current_roster[current_roster['Player'] != 'Team Totals']
+        current_roster.set_index('Player', inplace=True)
+        current_roster['Age'] = current_roster['Age'].apply(lambda x: int(x))
+
+        return current_roster
+        
 
     def __repr__(self):
         return f"<{self.abbreviation} - {self.franchise}>"
-
-
-sixers = NBAFranchise('PHI')
-sixers.players_all_time()
-print(sixers.url)
-print(sixers.players)
