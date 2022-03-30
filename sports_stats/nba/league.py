86a2da97cecac5c994b57deef8321c391a95a25f
alexkahan <amkahan@gmail.com>
2022-03-29 22:26:05 -0400

    Add abbreviations to self.teams

--- sports_stats/nba/league.py
+++ sports_stats/nba/league.py
@@ -14,12 +14,14 @@ class NBA:
         for item in soup.find_all(attrs={'data-stat': 'franch_name'})[1:31]:
             self.teams[item.find('a')['href'].split('/')[-2]] = {
                 "team_name": item.text,
+                "abbrev": item.find('a')['href'].split('/')[-2],
                 "url": self.url + item.find('a')['href'],
             }
+
 
     def franchise_codes(self):
         '''
         Print list of team codes, which are used for getting a specific franchise.
         '''
-        for abbrev, team_name in self.teams.items():
-            print(f"{abbrev} ({team_name['team_name']})")
+        for team in self.teams.items():
+            print(f"{team[1]['abbrev']} ({team[1]['team_name']})")
