0d60644c23d0567a612be9251bbbc3fa198dedae
alexkahan <amkahan@gmail.com>
2022-04-01 17:47:05 -0400

    Revert spellcheck

--- sports_stats/nba/helpers.py
+++ sports_stats/nba/helpers.py
@@ -1,5 +1,3 @@
-import os
-
 from sports_stats.nba.league import NBA
 from sports_stats.nba.team import NBAFranchise
 
@@ -12,7 +10,3 @@ def all_players():
         with open('sports_stats/assets/nba_players.txt', 'w', errors='ignore') as file:
             for player in players:
                 file.write(f'{player}\n')
-
-def spellcheck():
-    if not os.path.exists('sports_stats/assets/nba_players.txt'):
-            all_players()
