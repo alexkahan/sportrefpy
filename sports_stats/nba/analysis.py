bf65d14177be6d5c02a3a633a28c50ddc200fe15
alexkahan <amkahan@gmail.com>
2022-03-31 22:32:40 -0400

    Update player analysis to include more than 2 players and more than 1 stat. Update player model to include full name.

--- sports_stats/nba/analysis.py
+++ sports_stats/nba/analysis.py
@@ -2,40 +2,52 @@ from sports_stats.nba.player import NBAPlayer
 
 import pandas as pd
 
-def compare_players(player1, player2, stat=None, total='career'):
+from sports_stats.nba.team import NBAFranchise
+
+def compare_players(players, stats, total='career'):
     '''
     Compare regular season, post season, and career totals between two players.
     '''
 
-    p1 = NBAPlayer(player1)
-    p2 = NBAPlayer(player2)
-    if not stat:
+    players = [NBAPlayer(player) for player in players]
+    if not stats:
         if total == 'career':
-            comparison = pd.concat([p1.career_totals(), p2.career_totals()], axis=1)
-            comparison.columns = [player1, player2]
+            comparison = pd.concat([player.career_totals() for player in players], axis=1)
+            comparison.columns = [player.full_name for player in players]
         elif total == 'reg':
-            comparison = pd.concat([p1.regular_season_stats().sum(), p2.regular_season_stats().sum()], axis=1)
-            comparison.columns = [player1, player2]
+            comparison = pd.concat([player.regular_season_stats().sum() for player in players], axis=1)
+            comparison.columns = [player.full_name for player in players]
             comparison.drop(index={'Age', 'Tm', 'Lg', 'Pos', 'FG%', '3P%', '2P%', 'eFG%', 'FT%'}, inplace=True)
         elif total == 'post':
-            comparison = pd.concat([p1.post_season_stats().sum(), p2.post_season_stats().sum()], axis=1)
-            comparison.columns = [player1, player2]
+            comparison = pd.concat([player.post_season_stats().sum() for player in players], axis=1)
+            comparison.columns = [player.full_name for player in players]
             comparison.drop(index={'Age', 'Tm', 'Lg', 'Pos', 'FG%', '3P%', '2P%', 'eFG%', 'FT%'}, inplace=True)
-    elif stat:
+    elif stats:
         if total == 'career':
-            comparison = pd.concat([p1.career_totals().loc[stat], p2.career_totals().loc[stat]], axis=1)
-            comparison.columns = [player1, player2]
+            comparison = pd.concat([player.career_totals().loc[stats] for player in players], axis=1)
+            comparison.columns = [player.full_name for player in players]
         elif total == 'reg':
-            comparison = pd.concat([p1.regular_season_stats().sum(), p2.regular_season_stats().sum()], axis=1)
-            comparison.columns = [player1, player2]
+            comparison = pd.concat([player.regular_season_stats().sum() for player in players], axis=1)
+            comparison.columns = [player.full_name for player in players]
             comparison.drop(index={'Age', 'Tm', 'Lg', 'Pos', 'FG%', '3P%', '2P%', 'eFG%', 'FT%'}, inplace=True)
-            comparison = comparison.loc[stat]
+            comparison = comparison.loc[stats]
         elif total == 'post':
-            comparison = pd.concat([p1.post_season_stats().sum(), p2.post_season_stats().sum()], axis=1)
-            comparison.columns = [player1, player2]
+            comparison = pd.concat([player.post_season_stats().sum() for player in players], axis=1)
+            comparison.columns = [player.full_name for player in players]
             comparison.drop(index={'Age', 'Tm', 'Lg', 'Pos', 'FG%', '3P%', '2P%', 'eFG%', 'FT%'}, inplace=True)
-            comparison = comparison.loc[stat]
-        comparison['Diff'] = abs(comparison[player1] - comparison[player2])
+            comparison = comparison.loc[stats]
+        # comparison['Diff'] = abs(comparison[player1] - comparison[player2])
+        comparison = comparison.transpose()
+        comparison.columns = [stats]
         return comparison
     else:
         raise Exception
+
+
+def compare_franchises(teams):
+    teams = [NBAFranchise(team) for team in teams]
+    comparison = pd.concat([team.season_history()[['W', 'L']].sum() for team in teams], axis=1)
+    comparison.columns = [team.franchise_name for team in teams]
+    comparison = comparison.transpose()
+    comparison['W%'] = comparison['W'] / (comparison['W'] + comparison['L'])
+    return comparison
