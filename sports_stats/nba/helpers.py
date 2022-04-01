8ea507572dd643e13aade108fda1ac7716c76709
alexkahan <amkahan@gmail.com>
2022-04-01 10:32:38 -0400

    Add all_players for spelling suggestions

---
+++ sports_stats/nba/helpers.py
@@ -0,0 +1,12 @@
+from sports_stats.nba.league import NBA
+from sports_stats.nba.team import NBAFranchise
+
+def all_players():
+        players = set()
+        nba = NBA()
+        for team in nba.teams.keys():
+            franchise = NBAFranchise(team)
+            players.update(franchise.players_all_time_stats().index)
+        with open('sports_stats/assets/nba_players.txt', 'w', errors='ignore') as file:
+            for player in players:
+                file.write(f'{player}\n')
