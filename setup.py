4a8979c173d3d2d4b07b29c9d0d16994d14d3fe3
alexkahan <amkahan@gmail.com>
2022-04-01 22:17:55 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='sportrefpy',
+    author='alex kahan',
+    author_email='kahanscious@gmail.com',
+    version='0.0.1',
+    description='pull sports stats',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/alexkahan/sports_stats',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=["Programming Language :: Python :: 3"],
+    keywords='sports stats nba nfl mlb cfb cbb',
+    install_requires=[
+        'bs4 >= 0.0.1',
+        'lxml >= 4.8.0',
+        'numpy >= 1.22.3',
+        'pandas >= 1.4.1',
+        'pytest >= 7.1.1',
+        'requests >= 2.27.1',
+        'urllib3 >= 1.26.9'
+    ]
+)
