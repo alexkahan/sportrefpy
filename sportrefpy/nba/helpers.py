83beb9479082396d944732f949a4b3656e04837c
alexkahan <amkahan@gmail.com>
2022-04-02 10:44:41 -0400

    Update file path for nba_players dict

--- sportrefpy/nba/helpers.py
+++ sportrefpy/nba/helpers.py
@@ -9,7 +9,7 @@ def all_players():
         for team in nba.teams.keys():
             franchise = NBAFranchise(team)
             players.update(franchise.players_all_time_stats().index)
-        with open(os.path.dirname(__file__) + '/sportrefpy/assets/nba_players.txt', 'w', encoding='ascii') as file:
+        with open(os.path.dirname(os.path.dirname(__file__)) + '\\assets\\nba_players.txt', 'w', encoding='ascii') as file:
             for player in players:
                 try:
                     file.write(f'{player}\n')
--- sportrefpy/nba/player.py
+++ sportrefpy/nba/player.py
@@ -14,7 +14,7 @@ class NBAPlayer(NBA):
     def __init__(self, player):
         super().__init__()
         
-        self.player_dict = enchant.PyPWL(os.path.dirname(__file__) + '/sportrefpy/assets/nba_players.txt')
+        self.player_dict = enchant.PyPWL(os.path.dirname(os.path.dirname(__file__)) + '\\assets\\nba_players.txt')
         first_letter = player.split()[-1][0].lower()
         players = pd.read_html(self.url + f'/players/{first_letter}')[0]
         players['Player'] = players['Player'].apply(lambda x: x.split('*')[0])
