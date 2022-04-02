ad987f74fd3bb5cd3601872bc4e665b7714da5a7
alexkahan <amkahan@gmail.com>
2022-04-02 01:00:54 -0400

    fix asset path

--- sportrefpy/nba/helpers.py
+++ sportrefpy/nba/helpers.py
@@ -7,7 +7,7 @@ def all_players():
         for team in nba.teams.keys():
             franchise = NBAFranchise(team)
             players.update(franchise.players_all_time_stats().index)
-        with open('sports_stats/assets/nba_players.txt', 'w', encoding='ascii') as file:
+        with open('sportrefpy/assets/nba_players.txt', 'w', encoding='ascii') as file:
             for player in players:
                 try:
                     file.write(f'{player}\n')
