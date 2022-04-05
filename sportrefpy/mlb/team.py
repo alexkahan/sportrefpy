9008fd93ef2e1c5611723efb8be2dccf81a08a21
alexkahan <amkahan@gmail.com>
2022-04-04 21:48:59 -0400

    Add roster and individual team stats

--- sportrefpy/mlb/team.py
+++ sportrefpy/mlb/team.py
@@ -11,42 +11,53 @@ class MLBFranchise(MLB):
         self.batters_url = self.team_url + 'bat.shtml'
         self.pitchers_url = self.team_url + 'pitch.shtml'
         self.managers_url = self.team_url + 'managers.shtml'
-        self.current_roster_url = f'{self.url}/contracts/{self.abbreviation}.html'
         self.seasons_url = f'{self.url}/teams/{self.abbreviation}'
 
     
-    def batters_all_time_stats(self):
+    def batters_all_time_stats(self, batter=None):
         '''
         Returns Pandas dataframe of all historical player data.
         '''
 
-        players = pd.read_html(self.batters_url)[0]
-        players.drop(columns={'S', 'C', 'F'}, inplace=True)
-        players = players[players.columns[:-1]]
-        players.dropna(axis='rows', subset='Name', inplace=True)
-        players = players[players['Name'] != 'Name']
-        players['Name'] = players['Name'].apply(lambda x: x.split(' HOF')[0])
-        players.set_index('Name', inplace=True)
-        players = players.apply(pd.to_numeric)
-        
-        return players
+        batters = pd.read_html(self.batters_url)[0]
+        batters.drop(columns={'S', 'C', 'F', 'Rk'}, inplace=True)
+        batters = batters[batters.columns[:-1]]
+        batters.dropna(axis='rows', subset='Name', inplace=True)
+        batters = batters[batters['Name'] != 'Name']
+        batters['Name'] = batters['Name'].apply(lambda x: x.split(' HOF')[0])
+        batters.set_index('Name', inplace=True)
+        batters = batters.apply(pd.to_numeric)
 
-    def pitchers_all_time_stats(self):
+        if batter is not None:
+            try:
+                return batters.loc[batter]
+            except KeyError:
+                return 'Player not found.'
+        
+        return batters
+
+    def pitchers_all_time_stats(self, pitcher=None):
         '''
         Returns Pandas dataframe of all historical player data.
         '''
 
         pitchers = pd.read_html(self.pitchers_url)[0]
-        pitchers.drop(columns={'S', 'C', 'F'}, inplace=True)
+        pitchers.drop(columns={'S', 'C', 'F', 'Rk'}, inplace=True)
         pitchers.dropna(axis='rows', subset='Name', inplace=True)
         pitchers = pitchers[pitchers['Name'] != 'Name']
         pitchers['Name'] = pitchers['Name'].apply(lambda x: x.split(' HOF')[0])
         pitchers.set_index('Name', inplace=True)
         pitchers = pitchers.apply(pd.to_numeric)
+
+        if pitcher is not None:
+            try:
+                return pitchers.loc[pitcher]
+            except KeyError:
+                return 'Player not found.'
         
         return pitchers
 
-    def managers_all_time_data(self):
+    def managers_all_time_stats(self, manager=None):
         '''
         Returns Pandas dataframe of all historical coach data.
         '''
@@ -59,26 +70,32 @@ class MLBFranchise(MLB):
         managers.set_index('Mgr', inplace=True)
         managers = managers.apply(pd.to_numeric)
 
+        if manager is not None:
+            try:
+                return managers.loc[manager]
+            except KeyError:
+                return 'Player not found.'
+
         return managers
 
 
-    # def current_roster(self):
-    #     '''
-    #     Returns Pandas dataframe of current roster.
-    #     '''
+    def roster(self, season=None):
+        '''
+        Returns Pandas dataframe of roster for a given year.
+        '''
+        roster_url = f'{self.team_url}{str(season)}-roster.shtml'
+        roster = pd.read_html(roster_url ,attrs={'id': 'appearances'})[0]
+        roster['Name'] = roster['Name'].apply(lambda x: x.split(' HOF')[0])
+        roster = roster[roster['Name'] != 'Name']
+        roster.set_index('Name', inplace=True)
+        roster.drop(columns={'Unnamed: 2'}, inplace=True)
+        roster.dropna(axis='columns', how='any', inplace=True)
+        roster = roster.apply(pd.to_numeric, errors='ignore')
 
-    #     current_roster = pd.read_html(self.current_roster_url)[0]
-    #     current_roster = current_roster[current_roster.columns[:2]]
-    #     current_roster.columns = ['Player', 'Age']
-    #     current_roster = current_roster[current_roster['Player']\
-    #          != 'Team Totals']
-    #     current_roster.set_index('Player', inplace=True)
-    #     current_roster['Age'] = current_roster['Age'].apply(lambda x: int(x))
-
-    #     return current_roster
+        return roster
 
     
-    def season_history(self):
+    def season_history(self, year=None):
         '''
         Returns Pandas dataframe of seasons.
         '''
@@ -92,6 +109,12 @@ class MLBFranchise(MLB):
         seasons['Playoffs'].replace('nan', np.nan, inplace=True)
         seasons.set_index('Year', inplace=True)
         seasons.drop(columns={'Tm'}, inplace=True)
+
+        if year is not None:
+            try:
+                return seasons.loc[year]
+            except KeyError:
+                return 'Season not found.'
 
         return seasons
 
